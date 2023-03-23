0ee8136d40f5e599b46de2c6437b7c9a90be1c43
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-23 16:13:57 +0300

    Add settings env var

--- src/crowdfunding_platform/settings.py
+++ src/crowdfunding_platform/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$7hj-5)(5ty$vjor(#xqu!h&!q(v*iwta%2rggi)bq*i4-vt#y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
