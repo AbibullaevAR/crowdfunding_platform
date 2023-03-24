229419a6c823bdce4cd64c160fd7e4b4442cb4da
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-24 18:52:39 +0300

    Create Role model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -4,6 +4,20 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Role(models.Model):
+
+    class RoleInSystem(models.TextChoices):
+        ADMIN = 'AD', _('Admin')
+        STUDENT = 'ST', _('Student')
+
+    role_in_system = models.CharField(
+        max_length=2,
+        choices=RoleInSystem.choices,
+        default=RoleInSystem.STUDENT,
+        blank=False
+    )
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     name = models.CharField(_('name'), max_length=60, blank=False)
