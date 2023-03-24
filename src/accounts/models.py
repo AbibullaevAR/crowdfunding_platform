ad9c319c8ddcc8fe4a64f8a843aa04508b289dc1
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-24 19:02:39 +0300

    Create User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
+from django.core.mail import send_mail
+
+from .managers import UserManager
 
 # Create your models here.
 
@@ -22,3 +25,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True)
     name = models.CharField(_('name'), max_length=60, blank=False)
     is_active = models.BooleanField(_('active'), default=True)
+
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def email_user(self, subject: str, message: str, from_email=None, **kwargs) -> None:
+        '''
+        Sends an email to this User.
+        '''
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+        
