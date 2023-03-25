a735f90c9ec2b65ebadff8916423f0f84e813b0f
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-25 21:49:04 +0300

    Add AUTH_USER_MODEL

--- src/crowdfunding_platform/settings.py
+++ src/crowdfunding_platform/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -134,4 +135,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.UUIDField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
