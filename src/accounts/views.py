77a1cd0497ec051b54b6148c4741060f29de7758
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-26 15:28:04 +0300

    add create user view

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .serializers import CreateUserSerializer
+from .services import CreateUserService
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = CreateUserSerializer
+
+    def perform_create(self, serializer):
+        CreateUserService().execute(**serializer.validated_data)
