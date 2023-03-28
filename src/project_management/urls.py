419eb70a231693e66e68169c15214a42bf04a87e
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-28 20:05:44 +0300

    add url for list category

--- src/project_management/urls.py
+++ src/project_management/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CreateProjectView
+from .views import CreateProjectView, ListCategoryView
 
 app_name = 'project_management'
 
 urlpatterns = [
-    path("create_project/",  CreateProjectView.as_view(), name="create_project")
+    path("create_project/",  CreateProjectView.as_view(), name="create_project"),
+    path("list_category/", ListCategoryView.as_view(), name="list_category")
 ]
