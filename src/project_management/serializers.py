416a4063c21eb5e6c725cdedd1973f8568132b90
Exe7hnik <43046031+Exe7hnik@users.noreply.github.com>
2023-03-28 22:07:59 +0300

    fix: create project be able set categories

--- src/project_management/serializers.py
+++ src/project_management/serializers.py
@@ -5,6 +5,8 @@ from project_management.models import Project, ProjectCategory, Category
 
 class CreateProjectSerializer(serializers.ModelSerializer):
 
+    categories = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True)
+
     class Meta:
         model = Project
         fields = ('title', 'goal_likes', 'short_description', 'start_project', 'end_project', 'categories')
