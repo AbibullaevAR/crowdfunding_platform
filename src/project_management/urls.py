2ecb25f6badbe1fffaef7edf8cf710fe21a147e3
Abibullaev <a.alim2019@mail.ru>
2023-04-04 10:37:17 +0300

    add url for like_project

--- src/project_management/urls.py
+++ src/project_management/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import CreateProjectView, ListCategoryView, RetrieveProjectView
+from .views import CreateProjectView, ListCategoryView, RetrieveProjectView, LikeProjectView
 
 app_name = 'project_management'
 
 urlpatterns = [
     path("create_project/",  CreateProjectView.as_view(), name="create_project"),
     path("retrieve_project/", RetrieveProjectView.as_view(), name="retrieve_project"),
+    path("like_project/", LikeProjectView.as_view(), name="like_project"),
     path("list_category/", ListCategoryView.as_view(), name="list_category")
 ]
