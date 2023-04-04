07c74d81dc1cb71da4763b350b59f116735e8fcb
Abibullaev <a.alim2019@mail.ru>
2023-04-04 10:39:03 +0300

    create LikeProjectView

--- src/project_management/views.py
+++ src/project_management/views.py
@@ -1,8 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.request import Request
 
 from project_management.models import Category, Project
-from project_management.serializers import CreateProjectSerializer, CategorySerializer, RetrieveProjectSerializer
+from project_management.serializers import CreateProjectSerializer, CategorySerializer, RetrieveProjectSerializer, LikeProjectSerializer
+from project_management.services import like_project
 
 # Create your views here.
 
@@ -27,3 +30,16 @@ class ListCategoryView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
+class LikeProjectView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = LikeProjectSerializer
+
+    def post(self, request: Request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        like_project(serializer.validated_data.get('project_id'), request.user)
+
+        return Response(status=status.HTTP_202_ACCEPTED)
