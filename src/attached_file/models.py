58b8dff779eb3a4119e0afda37ea60e6d82ff9fc
Abibullaev <a.alim2019@mail.ru>
2023-04-05 17:52:45 +0300

    create Image model

--- src/attached_file/models.py
+++ src/attached_file/models.py
@@ -1,3 +1,21 @@
+import uuid
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from project_management.models import Project
 
 # Create your models here.
+
+class Image(models.Model):
+
+    AVAILABLE_FORMAT_CHOICES = (
+        ('.png', 'PNG'),
+        ('.jpg', 'JPG'),
+        ('.jpge', 'JPGE')
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    available_format = models.CharField(_('available_format'), max_length=5, choices=AVAILABLE_FORMAT_CHOICES, default='.jpg')
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
