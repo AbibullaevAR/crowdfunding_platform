ddd0622592bc8f6c09de8380bdebe001d501455a
Abibullaev <a.alim2019@mail.ru>
2023-04-05 16:29:03 +0300

    add External storage settings

--- src/crowdfunding_platform/settings.py
+++ src/crowdfunding_platform/settings.py
@@ -164,3 +164,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
 EMAIL_USE_TLS = True
+
+# External storage settings
+EXTERNAL_STORAGE_TOKEN = os.environ.get('EXTERNAL_STORAGE_TOKEN')
