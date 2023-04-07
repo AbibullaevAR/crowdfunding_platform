6d69bc7eddc92d0719d17d322941ed7f3c4ee4ac
Abibullaev <a.alim2019@mail.ru>
2023-04-07 14:32:50 +0300

    add field img_links

--- src/project_management/serializers.py
+++ src/project_management/serializers.py
@@ -15,13 +15,18 @@ class CreateProjectSerializer(serializers.ModelSerializer):
 
 
 class RetrieveProjectSerializer(serializers.ModelSerializer):
+
+    img_links = serializers.SerializerMethodField()
     
     taken_likes = serializers.IntegerField(source='taken_likes_count')
     categories = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True)
 
     class Meta:
         model = Project
-        fields = ('id', 'taken_likes', 'title', 'goal_likes', 'short_description', 'start_project', 'end_project', 'categories')
+        fields = ('id', 'taken_likes', 'title', 'goal_likes', 'short_description', 'start_project', 'end_project', 'categories', 'img_links')
+    
+    def get_img_links(self, instance):
+        return self.context.get('img_links')
 
 
 class CategorySerializer(serializers.ModelSerializer):
