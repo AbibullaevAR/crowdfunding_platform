b3cbb97e19bffb27888a4cee4823d68898ca8ea8
Abibullaev <a.alim2019@mail.ru>
2023-04-07 14:33:13 +0300

    add method get_download_link_for_images

--- src/attached_file/services.py
+++ src/attached_file/services.py
@@ -7,3 +7,9 @@ def create_image_for_project(project, available_formats: list[str]) -> list[str]
     images: list[Image] = [Image.objects.create(project=project, available_format=available_format) for available_format in available_formats]
     storage = ExternalStorageManage()
     return [storage.get_upload_link(str(image.id) + image.available_format) for image in images]
+
+
+def get_download_link_for_images(images: list[Image]) -> list[str]:
+
+    storage = ExternalStorageManage()
+    return [storage.get_download_link(str(image.id) + image.available_format) for image in images]
