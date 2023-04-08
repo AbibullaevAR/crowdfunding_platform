9a6a160fca49e46e58bc74dc906339702f1bbf37
Abibullaev <a.alim2019@mail.ru>
2023-04-08 21:33:08 +0300

    add path for list_project

--- src/project_management/urls.py
+++ src/project_management/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
-from .views import CreateProjectView, ListCategoryView, RetrieveProjectView, LikeProjectView, ChangeProjectStatusView
+from .views import (
+    CreateProjectView, 
+    ListCategoryView, 
+    RetrieveProjectView, 
+    LikeProjectView, 
+    ChangeProjectStatusView,
+    ListApproveProjectView
+    )
 
 app_name = 'project_management'
 
 urlpatterns = [
     path("create_project/",  CreateProjectView.as_view(), name="create_project"),
     path("retrieve_project/", RetrieveProjectView.as_view(), name="retrieve_project"),
+    path("list_project/", ListApproveProjectView.as_view(), name="list_project"),
     path("like_project/", LikeProjectView.as_view(), name="like_project"),
     path("list_category/", ListCategoryView.as_view(), name="list_category"),
 
