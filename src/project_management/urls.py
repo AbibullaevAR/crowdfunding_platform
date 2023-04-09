bd5c8b4f3283507b43421aefa147fe9920d0b580
Abibullaev <a.alim2019@mail.ru>
2023-04-09 12:19:07 +0300

    create list_user_project url

--- src/project_management/urls.py
+++ src/project_management/urls.py
@@ -6,7 +6,8 @@ from .views import (
     RetrieveProjectView, 
     LikeProjectView, 
     ChangeProjectStatusView,
-    ListApproveProjectView
+    ListApproveProjectView,
+    ListUserProjectView
     )
 
 app_name = 'project_management'
@@ -15,6 +16,7 @@ urlpatterns = [
     path("create_project/",  CreateProjectView.as_view(), name="create_project"),
     path("retrieve_project/", RetrieveProjectView.as_view(), name="retrieve_project"),
     path("list_project/", ListApproveProjectView.as_view(), name="list_project"),
+    path("list_user_project/", ListUserProjectView.as_view(), name="list_user_project"),
     path("like_project/", LikeProjectView.as_view(), name="like_project"),
     path("list_category/", ListCategoryView.as_view(), name="list_category"),
 
