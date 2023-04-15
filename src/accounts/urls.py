611d5d6bfc4d68e8016d454384f497a2a8e9d84f
Abibullaev <a.alim2019@mail.ru>
2023-04-15 18:03:47 +0300

    add path for user_status

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -5,16 +5,19 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from .views import CreateUserView, ConfirmEmailView, RetrieveUserView, ListUserView
+from .views import CreateUserView, ConfirmEmailView, RetrieveUserView, ListUserView, UpdateUserStatusView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path("create_user/", CreateUserView.as_view(), name="create_user"),
     path("retrieve_user/", RetrieveUserView.as_view(), name="retrieve_user"),
-    path("users/", ListUserView.as_view(), name="users_list"),
     path('confirm_email/<token>', ConfirmEmailView.as_view(), name='confirm_email'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify")
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+
+    # Admin path
+    path("users/", ListUserView.as_view(), name="users_list"),
+    path("user_status/<uuid:id>/", UpdateUserStatusView.as_view(), name="user_status")
 ]
