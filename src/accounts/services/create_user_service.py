905e93a85a6c16f0c680d425f04741e8b5319bb1
AbibullaevAR <a.alim2019@mail.ru>
2023-05-09 10:21:57 +0300

    fix: set correct url on email user

--- src/accounts/services/create_user_service.py
+++ src/accounts/services/create_user_service.py
@@ -1,5 +1,7 @@
 
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.conf import settings
 
 from accounts.utilities import generate_reg_token
 from .helpers import UserEntity, UsersDAO
@@ -16,7 +18,7 @@ class CreateUserService:
 
         token = generate_reg_token(user_entity.id)
 
-        url = view.get_absolute_uri('accounts:confirm_email', token=token)
+        url = settings.FRONTEND_URL + reverse('accounts:confirm_email',  kwargs={'token':token})
 
         user_DAO.email_user(
             user_entity, 
